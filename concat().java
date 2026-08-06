0094a5f7a6ee1c0545ab1bb216582818f065d910
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:17:38 +0530

    Add ConcatExample class to concatenate strings

---
+++ concat().java
@@ -0,0 +1,8 @@
+public class ConcatExample {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = " World";
+
+        System.out.println(str1.concat(str2));
+    }
+}
