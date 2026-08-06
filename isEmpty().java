044e8dd8ca3904542d8d7e41acb6c8435fddc6f5
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:24:22 +0530

    Add IsEmptyExample class to check string emptiness

---
+++ isEmpty().java
@@ -0,0 +1,11 @@
+public class IsEmptyExample {
+    public static void main(String[] args) {
+
+        String str1 = "";
+        String str2 = "Java";
+
+        System.out.println(str1.isEmpty());
+        System.out.println(str2.isEmpty());
+
+    }
+}
