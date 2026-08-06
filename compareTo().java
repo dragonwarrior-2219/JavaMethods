46db8b4a7d0b3cbadd85292403a580fea516c00f
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:16:53 +0530

    Add CompareToExample class for string comparison

---
+++ compareTo().java
@@ -0,0 +1,8 @@
+public class CompareToExample {
+    public static void main(String[] args) {
+        String str1 = "Apple";
+        String str2 = "Banana";
+
+        System.out.println(str1.compareTo(str2));
+    }
+}
