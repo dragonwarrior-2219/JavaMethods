be209b0727ca1d7c3dd925dbf1c5c01652df2b0d
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:18:08 +0530

    Create contains().java

---
+++ contains().java
@@ -0,0 +1,7 @@
+public class ContainsExample {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println(str.contains("Program"));
+    }
+}
