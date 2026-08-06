179f4b28a108d2887ce2bbd8d91367f381afc38f
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:28:28 +0530

    Add SubSequenceExample class to demonstrate subSequence

---
+++ subSequence().java
@@ -0,0 +1,11 @@
+public class SubSequenceExample {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        CharSequence result = str.subSequence(5, 16);
+
+        System.out.println(result);
+
+    }
+}
