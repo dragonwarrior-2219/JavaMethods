8d8b46e83739dd2e13036da12ecca746c808d229
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:16:26 +0530

    Add CodePointCountExample class with main method

---
+++ codePointCount().java
@@ -0,0 +1,6 @@
+public class CodePointCountExample {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.codePointCount(0, str.length()));
+    }
+}
