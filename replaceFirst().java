96629c785a0edaaf82214f9c00f62a66831d703d
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:27:27 +0530

    Add ReplaceFirstExample class to replace first occurrence

---
+++ replaceFirst().java
@@ -0,0 +1,9 @@
+public class ReplaceFirstExample {
+    public static void main(String[] args) {
+
+        String str = "Java Java Java";
+
+        System.out.println(str.replaceFirst("Java", "Python"));
+
+    }
+}
