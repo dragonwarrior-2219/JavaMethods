6c71f864bcc37649424ee0e3ad02955b22781e99
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:32:16 +0530

    Add ValueOfExample class to demonstrate String.valueOf

---
+++ valueOf().java
@@ -0,0 +1,11 @@
+public class ValueOfExample {
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        String str = String.valueOf(number);
+
+        System.out.println(str);
+
+    }
+}
