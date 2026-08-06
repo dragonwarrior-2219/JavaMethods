df66384c034c9dcff1ed92c4729b109f424670be
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:21:14 +0530

    Add FormatExample class for formatted output

---
+++ format().java
@@ -0,0 +1,7 @@
+public class FormatExample {
+    public static void main(String[] args) {
+        String str = String.format("Name: %s, Age: %d", "Aakash", 21);
+
+        System.out.println(str);
+    }
+}
