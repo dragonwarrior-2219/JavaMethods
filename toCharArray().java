3c5d99a5ed4877de42bcf8b37f8602e5399b6761
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:29:13 +0530

    Add ToCharArrayExample class to demonstrate toCharArray

---
+++ toCharArray().java
@@ -0,0 +1,13 @@
+public class ToCharArrayExample {
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        char[] ch = str.toCharArray();
+
+        for (char c : ch) {
+            System.out.println(c);
+        }
+
+    }
+}
