a238efed1879a4542bd643026ae88ce8cd1ad1d8
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:27:47 +0530

    Add StartsWithExample class to demonstrate startsWith()

---
+++ startsWith().java
@@ -0,0 +1,9 @@
+public class StartsWithExample {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        System.out.println(str.startsWith("Java"));
+
+    }
+}
