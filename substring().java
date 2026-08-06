ef2dddd781b78838ad17f3afb6ebb7108187a16e
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:28:49 +0530

    Add SubStringExample class for substring demonstration

---
+++ substring().java
@@ -0,0 +1,9 @@
+public class SubStringExample {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        System.out.println(str.substring(5));
+
+    }
+}
