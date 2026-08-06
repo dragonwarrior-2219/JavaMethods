25c345753442876546c753496c3402767432945d
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:20:28 +0530

    Add EqualsIgnoreCaseExample class with main method

---
+++ equalsIgnoreCase().java
@@ -0,0 +1,8 @@
+public class EqualsIgnoreCaseExample {
+    public static void main(String[] args) {
+        String str1 = "JAVA";
+        String str2 = "java";
+
+        System.out.println(str1.equalsIgnoreCase(str2));
+    }
+}
