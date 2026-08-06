8322f600814837075785620e79e22e4d32ae3b20
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:17:16 +0530

    Add CompareToIgnoreCaseExample class

---
+++ compareToIgnoreCase().java
@@ -0,0 +1,8 @@
+public class CompareToIgnoreCaseExample {
+    public static void main(String[] args) {
+        String str1 = "JAVA";
+        String str2 = "java";
+
+        System.out.println(str1.compareToIgnoreCase(str2));
+    }
+}
