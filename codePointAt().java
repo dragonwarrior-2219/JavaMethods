5b12667575dcca12709b8e301369edfcf2432683
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:14:41 +0530

    Add codePointAt() example in CodePointAtExample class

---
+++ codePointAt().java
@@ -0,0 +1,7 @@
+// codePointAt() Example
+public class CodePointAtExample {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.codePointAt(1));
+    }
+}
