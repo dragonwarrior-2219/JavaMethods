c5c5a3e4593df426bca27bc2c0470002b80262b6
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:24:46 +0530

    Add JoinExample class to demonstrate String.join()

---
+++ join().java
@@ -0,0 +1,9 @@
+public class JoinExample {
+    public static void main(String[] args) {
+
+        String str = String.join("-", "Java", "Python", "C++");
+
+        System.out.println(str);
+
+    }
+}
