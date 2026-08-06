ca51b875237e9a088687db58628b704ca3b53369
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:30:54 +0530

    Add ToUpperCaseExample class to demonstrate toUpperCase

---
+++ toUpperCase().java
@@ -0,0 +1,9 @@
+public class ToUpperCaseExample {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        System.out.println(str.toUpperCase());
+
+    }
+}
