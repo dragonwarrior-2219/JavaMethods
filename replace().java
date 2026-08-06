5c41fccf57156b4e81d9b1d8b6eb3f033ab0f2b9
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:26:24 +0530

    Add ReplaceExample class to demonstrate string replacement

---
+++ replace().java
@@ -0,0 +1,9 @@
+public class ReplaceExample {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        System.out.println(str.replace("Java", "Python"));
+
+    }
+}
