be5460c3a6610e3eca9987fb02346ee636519bda
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:19:38 +0530

    Add EndsWithExample class to demonstrate endsWith method

---
+++ endsWith().java
@@ -0,0 +1,7 @@
+public class EndsWithExample {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println(str.endsWith("Programming"));
+    }
+}
