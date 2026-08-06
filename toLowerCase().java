4bc9932ca06ec948cdd992a8836ffddb2c278b49
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:29:36 +0530

    Add ToLowerCaseExample class to demonstrate toLowerCase

---
+++ toLowerCase().java
@@ -0,0 +1,9 @@
+public class ToLowerCaseExample {
+    public static void main(String[] args) {
+
+        String str = "JAVA Programming";
+
+        System.out.println(str.toLowerCase());
+
+    }
+}
