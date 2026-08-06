1fad9648c3531da1929d8e14366f1d68e61fde69
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:26:54 +0530

    Add ReplaceAllExample class to demonstrate string replacement

---
+++ replaceAll().java
@@ -0,0 +1,9 @@
+public class ReplaceAllExample {
+    public static void main(String[] args) {
+
+        String str = "a1b2c3";
+
+        System.out.println(str.replaceAll("\\d", "*"));
+
+    }
+}
