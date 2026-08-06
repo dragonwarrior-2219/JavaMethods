566b248f63a2f620d48686dc31c4b1bd2c1bfa63
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-06 14:23:47 +0530

    Add IndexOfExample class to demonstrate indexOf usage

---
+++ indexOf().java
@@ -0,0 +1,7 @@
+public class IndexOfExample {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println(str.indexOf("Programming"));
+    }
+}
